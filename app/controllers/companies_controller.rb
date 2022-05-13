53f23abbf34261fc7b5129bd05e92c27e28f156b
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-05-13 18:48:21 +0000

    1

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,12 +1,8 @@
 class CompaniesController < ApplicationController
 
-  # TODO:
-  # def index
   def index
   @companies = Company.all
   end
-
-  # end
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
