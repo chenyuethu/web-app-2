546f4f963ed24902c2701d6bdfed42add97ae7eb
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-05-13 21:03:11 +0000

    1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,18 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+    @post.save 
+  end
+
+  def update
+    @post = Post.new
+    @post["author"] = params["post"]["author"]
+    @post["body"] = params["post"]["body"]
+    @post["image"] = params["post"]["image"]
+    @post.save
+    redirect_to "/posts"
+  end
+
 end
